6579994c22d67c5954003716c1de86c990a044c9
ECHO <sir.alkatras@bk.ru>
2024-09-26 14:00:07 +0300

    version 1.1

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// Параллакс-эффект
+// Параллакс-эффект и прокрутка
 window.addEventListener('scroll', function() {
-    const parallaxSections = document.querySelectorAll('.parallax-section');
-    
-    parallaxSections.forEach((section) => {
-      let scrollPosition = window.scrollY;
-      section.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
-    });
-  });
+  const parallaxSections = document.querySelectorAll('.parallax-section');
   
+  parallaxSections.forEach((section) => {
+    let scrollPosition = window.scrollY;
+    section.style.backgroundPositionY = `${scrollPosition * 0.5}px`;
+  });
+});
+
+// Прокрутка вниз по нажатию на кнопку
+function scrollDown() {
+  const aboutSection = document.getElementById('about');
+  aboutSection.scrollIntoView({ behavior: 'smooth' });
+}
